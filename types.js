3c8f1765ddc4b6f243d281f58713ccfce1e1cebe
alamin-dev0 <alamin.dev01@gmail.com>
2025-09-09 18:47:19 +0600

    added some code

--- types.js
+++ types.js
@@ -2,7 +2,19 @@
 
 var name = "John Doe";
 var address = "123 Main St";
-console.log(name);
-console.log(address);
+// console.log(name);
+// console.log(address);
 var country = "Bangladesh";
 
+
+// boolean type variable
+var isRich = true;
+console.log(isRich);
+var passed = false;
+console.log(typeof passed);
+console.log(passed);
+console.log(typeof country)
+
+// number type variable
+var price = 50.5;
+console.log(typeof price);
