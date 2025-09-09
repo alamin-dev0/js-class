1f37a34d2b2d4218ff8fdf1b89719162c5e33f47
alamin-dev0 <alamin.dev01@gmail.com>
2025-09-09 17:54:36 +0600

    added some code

---
+++ variable.js
@@ -0,0 +1,19 @@
+/**
+ * variable == > vary + able
+ * 
+ * 5 things to declare a variable 
+ */
+
+var weight = 70; 
+var price = 50;
+var year = 2024;
+var money = 1000;
+money = 2000;
+
+var age = 'ddk';
+
+console.log(money);
+// console.log(year);
+// console.log(price);
+// console.log(weight);
+// console.log(age);
