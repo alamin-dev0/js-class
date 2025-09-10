911b1be3d56ac383fa37795a302dfd7faebd56dd
alamin-dev0 <alamin.dev01@gmail.com>
2025-09-10 18:51:41 +0600

    added nameing file

---
+++ nameing.js
@@ -0,0 +1,27 @@
+// 1. no keyword in variable name 
+var false = 34;
+var var = 45;
+//  2. no space or gap in variable name
+var my name = "John";
+var is happy = true;
+3. no quotation in variable name
+var "name" = "John";
+var 'address' = "123 Main St";
+// 4. no special character in variable name except _ and $
+var @home = "My Home";
+var #1stPlace = "First Place";
+// 5. variable name cannot start with a number but can end with a number
+var 1stName = "John";
+var 2ndAddress = "456 Another St";
+// 6. variable name is case sensitive
+var Name = "John";
+var name = "Doe";
+// 7. name is case sensitive
+address = "123 Main St";
+Address = "456 Another St";
+// 8. how to write a long variable name
+var my current home address = "123 Main St, City, Country";
+var mycurrenthomeaddress = "123 Main St, City, Country";
+var my_current_home_address = "123 Main St, City, Country"; //snake case 
+var myCurrentHomeAddress = "123 Main St, City, Country"; //camel case
+var MyCurrentHomeAddress = "123 Main St, City, Country"; //pascal case
